ecf0b88bbbe2b0d20263f8006a154887e4a0aeaf
Azure May Burmeister <azure.may.burmeister@gmail.com>
2020-04-30 14:42:52 -0700

    complete app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Azure May Burmeister on 2/3/20.
+//  Copyright © 2020 Azure May Burmeister. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
